18dbb689216df698147faf95bfa16364922d8115
Alifilho <alissonoliveiram@gmail.com>
2020-05-26 22:37:39 -0300

    Login offline working

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  login = '';
+  password = '';
 
-  constructor() { }
+  constructor(private router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async handleLogin() {
+    if (this.login === 'admin' && this.password === 'root') {
+      this.router.navigate(['/equipe-medica']);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Login/Senha incorretos',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
 
 }
