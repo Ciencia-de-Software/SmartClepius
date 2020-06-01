cbdd3c19a812fbb96df38f31071a223d03135ca7
Alifilho <alissonoliveiram@gmail.com>
2020-06-01 18:16:01 -0300

    Responsavel

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,25 @@ export class LoginPage implements OnInit {
   login = '';
   password = '';
 
+  state$: Observable<object>;
+
   constructor(private router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
+    console.log(this.state$);
   }
 
   async handleLogin() {
+    if (this.login === 'user' && this.password === 'user') {
+      this.router.navigate(['/responsavel']);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Login/Senha incorretos',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+
     if (this.login === 'admin' && this.password === 'root') {
       this.router.navigate(['/equipe-medica']);
     } else {
