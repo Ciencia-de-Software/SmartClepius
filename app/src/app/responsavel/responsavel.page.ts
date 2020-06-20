285889af120bdb3a765b4a98c64d776d6a5c1384
Alifilho <alissonoliveiram@gmail.com>
2020-06-20 12:53:08 -0300

    after monorepo fixes

--- app/src/app/responsavel/responsavel.page.ts
+++ app/src/app/responsavel/responsavel.page.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-responsavel',
-  templateUrl: './responsavel.page.html',
-  styleUrls: ['./responsavel.page.scss'],
+  selector: "app-responsavel",
+  templateUrl: "./responsavel.page.html",
+  styleUrls: ["./responsavel.page.scss"],
 })
 export class ResponsavelPage implements OnInit {
-  pacient = 'Fulano';
-  bed = 'Cama 012';
-  status = 'Estável';
+  pacient = "Fulano";
+  bed = "Cama 012";
+  status = "Estável";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
